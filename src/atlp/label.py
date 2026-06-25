cf087769e891fa4462012f8bb5d2d8f0c77852a7
phakhawanonpinsiri <pakavanonpinsiri@gmail.com>
2026-06-25 10:30:16 +0700

    Fix uppercase bool in json loads

--- src/atlp/label.py
+++ src/atlp/label.py
@@ -57,7 +57,7 @@ prompt_deliverables = (
     "Output the result as a plain string in .json format."
     "(Do not use ```json, just output the plain text only)"
     # "(Do not include [] in front of the output string as well)"
-    "Specify instruction: (string), the tags (list of strings), and is_failed: (boolean).\n"
+    "Specify instruction: (string), the tags (list of strings), and is_failed: (boolean true or false, lowercase).\n"
 )
 
 
@@ -172,7 +172,8 @@ def label(
                 output_text = processor.batch_decode(
                     generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
                 )
-                if isinstance(output_text, list): output_text = output_text[0]
+                # if isinstance(output_text, list): output_text = output_text[0]
+                print(type(output_text[0]))
 
                 try:
                     output_json = json.loads(output_text[0])
