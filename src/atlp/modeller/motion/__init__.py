4fee697eb3a3baaeda16dc2856904c0bf1d20f49
phakhawanonpinsiri <pakavanonpinsiri@gmail.com>
2026-07-09 15:02:05 +0700

    Restructure ATLP, 2

---
+++ src/atlp/modeller/motion/__init__.py
@@ -0,0 +1,5 @@
+from .motion import (
+    is_self_collision,
+    is_self_collision_from_datapoint,
+    model_is_self_collision,
+)
---
+++ src/atlp/visualizer/__init__.py
@@ -0,0 +1,4 @@
+from .visualizer import (
+    plot_joint_states,
+    simulate_joint_arrays,
+)
