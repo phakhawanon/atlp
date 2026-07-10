8bca77292931b55f903c18fa67121dd4e9536c71
phakhawanonpinsiri <pakavanonpinsiri@gmail.com>
2026-07-10 16:45:00 +0700

    Update documentation

--- src/atlp/__init__.py
+++ src/atlp/__init__.py
@@ -1,25 +1,54 @@
 """
-    Summary of Automatic Task Labelling Pipeline (ATLP) Module
+    Automatic Task Labelling Pipeline (ATLP) Package
 
-    Currently support automatic task labelling from Galbot G1 teleoperation dataset
+    This python package is designed to act as a quality pipeline for galbot one golf foxtrot's teleoperation data.
+
+    Provided with this package are python functions which can be used to
+        - manage the teleoperation dataset. (atlp.core)
+        - automatically label the teleoperation dataset with English instruction. (atlp.modeller.label)
+        - automatically detect whether the given teleoperation data contains self-collision or not. (atlp.modeller.motion)
+        - visualize the teleoperation dataset (atlp.visualizer)
+        - evaluate the quality pipeline (atlp.evaluator)
+
+    This python package also supports manual labelling for the teleoperation dataset, 
+    though it is better to refer to ATLP GUI package for a more convenience GUI implementation of this package.
+
+    The package is also written so that it is easy to be extended. Namely, if you want to modify this package to
+        - accept data from any robot or platform, modify atlp.core.teleop_reader
+        - replace the current data storage that uses only header.json, modify atlp.core.interface
+        - add more automatic labelling pipelines (e.g. motion smoothness, or brightness), modify atlp.modeller
+        - add more visualization tool, modify atlp.visualizer
+        - add more means to evaluate the automatic labelling pipelines, modify atlp.evaluator
+
+    As the name suggested, all subpackages reference atlp.core. 
+    Therefore, correctly modifying the atlp.core do not break other subpackages.
+    
+    For the terms used in this package and repository, please refer to the README.md of this directory.
 
     Note:
+        - Importing atlp only imports atlp.core and atlp.visualizer.plotter.
+            If you want to use other subpackages, import them explicitly.
+            This is the case because other subpackages are more heavy and application-specific.
+
+    Example:
+        >>> import atlp # This import atlp.core and atlp.visualizer.plotter
+        >>> import atlp.modeller.label
+        >>> atlp.set_root(root_to_dataset)
+        >>> atlp.populate_header() # Track the data in the dataset
+        >>> atlp.list_datapoints() # Print all details of all datapoints
+        >>> atlp.show_statistics() # See how many unlabelled datapoints are there in the dataset
+        >>> atlp.modeller.label.label() # Label all unlabelled datapoints
+        >>> atlp.display_instruction() # Print all instructions of all datapoints
+
+        .. todo::
+        - Implement trivial error handling (input type mismatch, etc) for all functions
+
+    Note:
+        TBD Note
+
         Datapoint is a folder inside the dataset that ends with "_record0"
 
         The datapoint folder should at least contain camera_front_head_rgb.mp4, which is the RGB video output from the Galbot G1 head camera
-
-    Example:
-        >>> import atlp
-        >>> set_root(root_to_dataset)
-        >>> populate_header() # Track the data in the dataset
-        >>> list_datapoints() # Print all details of all datapoints
-        >>> show_statistics() # See how many unlabelled datapoints are there in the dataset
-        >>> label() # Label all unlabelled datapoints
-        >>> display_instruction() # Print all instructions of all datapoints
-
-    .. todo::
-        - Structure the code so it can be expanded vertically
-        - Implement trivial error handling (input type mismatch, etc) for all functions
 """
 
 from .core import *
--- src/atlp/core/__init__.py
+++ src/atlp/core/__init__.py
@@ -1,3 +1,29 @@
+"""
+    atlp.core package
+
+    This package contains all essential functions to manage, retrieve, and modify data from the teleoperation data.
+
+    There are three subpackages:
+        1. atlp.core.teleop_reader
+            Handle retrieving data from the teleoperation data, which includes
+                - joint states (from data.json)
+                - videos (from *.mp4)
+                - frequency and duration (from report.txt)
+        2. atlp.core.interface
+            Handle retrieving/modifying/managing ATLP fields. Currently use header.json to store all ATLP field values.
+        3. atlp.core.dataset_manager
+            Additional quality-of-life functions that use the functions from .teleop_reader and .interface.
+
+    Note:
+        - User is encouraged to modify this code to match the robot data they have. (modifying atlp.core.teleop_reader)
+        - Moreover, if the user finds that header.json is too slow for a larger dataset, 
+            they are encouraged to upgrade the ATLP fields storage. (modifying atlp.core.interface)
+
+    Warning:
+        Because the atlp.core is referenced by every other subpackage, please modify the code with caution.
+            Namely, every mandatory input and output of a function should be properly maintained when you edit the code.
+"""
+
 from .interface import (
     set_root,
     populate_header,
